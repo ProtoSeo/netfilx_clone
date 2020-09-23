1488c65b688ca449ec0082eb82153909b12eae86
SeoSeunghun <proto_seo@naver.com>
2020-09-23 16:31:57 +0900

    add home screen and topBar

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,10 +12,42 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    return TopBar();
+  }
+}
+
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('real home'),
-      ),
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,  //사이에 간격을 넣어주기 위해서 사용함
+        children: <Widget>[
+        Image.asset(
+          'images/bbongflix_logo.png',
+          fit: BoxFit.contain,
+          height: 25,
+        ),
+        Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
+            )),
+        Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
+            )),
+        Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가 찜한 콘텐츠',
+              style: TextStyle(fontSize: 14),
+            )),
+      ]),
     );
   }
 }
